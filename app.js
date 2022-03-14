1b9c317c8dffc22feea63ac3e220170c3c6c55c2
FxIvan <almendraivan210814@gmail.com>
2022-03-14 20:11:46 -0300

    make it better

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.set('puerto', process.env.NODE_PUERTO)
+app.set('puerto', process.env.NODE_PUERTO || 3001)
 
 app.get('/',(req,res)=>{
     res.send('Conexion correctamente')
